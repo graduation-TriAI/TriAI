6dad6b34a53e9ff0588502ca167df5ff85ca7b6f
Park-Soyeong416 <pegasus@ewha.ac.kr>
2026-05-06 18:24:07 +0900

    Update Experiment ver2

--- 그로쓰/work/gnss/plot_test_scatter.py
+++ 그로쓰/work/gnss/plot_test_scatter.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from shared.paths import GNSS_CROSS
 from shared.config import WIN, STRIDE
 
-EXPERIMENT = "cross_event_tohoku_train_noto_test_2026-05-06"
+EXPERIMENT = "cross_event_noto_train_tohoku_test_2026-05-06"
 DIST_KM = "25km"
 
 LOG_DIR = GNSS_CROSS / f"{WIN}_{STRIDE}" / "logs" / EXPERIMENT
